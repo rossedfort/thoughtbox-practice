99a19d8d2baaa06df95207953fa4890dfee0685e
rossedfort <rossedfort@gmail.com>
2016-02-29 13:51:52 -0700

    implement link crud

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,11 +1,16 @@
 class LinksController < ApplicationController
   def index
-    @link = Link.new
-    @links = Link.all
+    if current_user
+      @link = Link.new
+      @links = current_user.links
+    else
+      redirect_to login_path
+    end
   end
 
   def create
     link = Link.new(link_params)
+    link.user_id = current_user.id
     if link.save
       flash[:messages] = 'Link Saved!'
       redirect_to links_path
@@ -13,6 +18,27 @@ class LinksController < ApplicationController
       flash[:errors] = link.errors.full_messages.join(', ')
       redirect_to :back
     end
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    link = Link.find(params[:id])
+    if link.update(link_params)
+      flash[:messages] = "Link Updated"
+      redirect_to links_path
+    else
+      flash.now[:errors] = link.erros.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  def destroy
+    Link.delete(params[:id])
+    flash[:messages] = "Link Deleted"
+    redirect_to links_path
   end
 
   private
