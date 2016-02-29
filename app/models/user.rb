1b5d35441c45ce80057fda1d9389ba0dba61ed84
rossedfort <rossedfort@gmail.com>
2016-02-29 11:43:37 -0700

    add model tests to ensure validations are working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,59 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should be able to create" do
-    user = User.new(username: 'rossedfort',
-                    password: 'password',
-                    email: 'rossedfort@yahoo.com',
-                    password_confirmation: 'password')
+  def valid_user
+    {
+     username: 'rossedfort',
+     password: 'password',
+     email: 'rossedfort@yahoo.com',
+     password_confirmation: 'password'
+   }
+  end
+
+  test "should be valid with valid attributes" do
+    user = User.new(valid_user)
 
     assert user.valid?
   end
+
+  test "should not be valid with missing username" do
+    invalid_user = valid_user
+    invalid_user[:username] = nil
+    user = User.new(invalid_user)
+
+    assert user.invalid?
+  end
+
+  test "should not be valid with missing email" do
+    invalid_user = valid_user
+    invalid_user[:email] = nil
+    user = User.new(invalid_user)
+
+    assert user.invalid?
+  end
+
+  test "should not be valid with missing password" do
+    invalid_user = valid_user
+    invalid_user[:password] = nil
+    user = User.new(invalid_user)
+
+    assert user.invalid?
+  end
+
+  test "should not be valid with missing password confirmation" do
+    invalid_user = valid_user
+    invalid_user[:password_confirmation] = nil
+    user = User.new(invalid_user)
+
+    assert user.invalid?
+  end
+
+  test "should not be valid with mismatched password and password confirmation" do
+    invalid_user = valid_user
+    invalid_user[:password_confirmation] = "password111111"
+
+    user = User.new(invalid_user)
+    assert user.invalid?
+  end
+
 end
