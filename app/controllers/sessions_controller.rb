bae7d7f924fc3d75cf54ddb3f96834e5e7f9fe81
rossedfort <rossedfort@gmail.com>
2016-02-29 11:12:15 -0700

    change sessions#create to use .authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:session][:username])
-    if @user
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:messages] = "Welcome, #{@user.username}"
       redirect_to @user
